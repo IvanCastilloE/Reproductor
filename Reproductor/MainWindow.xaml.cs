0a72e40571b16e00dcb642e2a522a571ae27e71a
alumno <alumno@E103>
2020-01-30 12:43:29 -0700

    slider de volumen usando colume sample provider

--- Reproductor/MainWindow.xaml.cs
+++ Reproductor/MainWindow.xaml.cs
@@ -37,6 +37,9 @@ namespace Reproductor
         WaveOut output;
 
         bool dragging = false;
+
+        VolumeSampleProvider volume;
+
         public MainWindow()
         {
             InitializeComponent();
@@ -98,11 +101,16 @@ namespace Reproductor
                 if(txtRutaArchivo.Text != null && txtRutaArchivo.Text != string.Empty)
                 {
                     reader = new AudioFileReader(txtRutaArchivo.Text);
+
+                    volume = new VolumeSampleProvider(reader);
+                    volume.Volume = (float)(sldVolumen.Value);
                     output = new WaveOut();
                     output.DeviceNumber = cbDispositivoSalida.SelectedIndex;
                     output.PlaybackStopped += Output_PlaybackStopped;
-                    output.Init(reader);
+                    output.Init(volume);
                     output.Play();
+                    //Cambiar el volumen del output
+                    //output.Volume = (float)(sldVolumen.Value);
 
                     btnReproducir.IsEnabled = false;
                     btnDetener.IsEnabled = true;
@@ -156,6 +164,15 @@ namespace Reproductor
             if(reader != null && output !=null && output.PlaybackState != PlaybackState.Stopped)
             {
                 reader.CurrentTime = TimeSpan.FromSeconds(sldTiempo.Value);
+            }
+        }
+
+        private void SldVolumen_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            if (output != null && output.PlaybackState != PlaybackState.Stopped)
+            {
+                //output.Volume = (float)(sldVolumen.Value);
+                volume.Volume = (float)(sldVolumen.Value);
             }
         }
     }
